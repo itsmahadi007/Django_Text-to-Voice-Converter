3a4d963b3c8a7223b9109c6283445dfee945e226
Mahadi Hassan <itsmahadi@gmail.com>
2023-08-15 20:27:14 +0600

    update

--- backend/settings/app_vars.py
+++ backend/settings/app_vars.py
@@ -14,17 +14,6 @@ if Use_Docker is False:
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
-    FRONTEND_URL = os.getenv("FRONTEND_URL")
-
-    # AWS RDS
-    # DB_HOST = os.getenv("DB_HOST")
-    DB_HOST = "localhost"
-    # DB_PORT = os.getenv("DB_PORT")
-    DB_PORT = 5432
-    DB_NAME = os.getenv("DB_NAME")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-
     # Redis
     REDIS_HOST = "localhost"
     REDIS_PORT = 6379
@@ -32,13 +21,6 @@ if Use_Docker is False:
     # CELERY settings
     CELERY_BROKER_URL = "redis://{}:{}/0".format(REDIS_HOST, REDIS_PORT)
     CELERY_RESULT_BACKEND = "redis://{}:{}/0".format(REDIS_HOST, REDIS_PORT)
-
-    # Email
-    EMAIL_HOST = os.getenv("EMAIL_HOST")
-    EMAIL_PORT = os.getenv("EMAIL_PORT")
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
 
     # Secret_key
     # SECURITY WARNING: keep the secret key used in production secret!
@@ -51,13 +33,6 @@ else:
 
     FRONTEND_URL = os.environ.get("FRONTEND_URL")
 
-    # DB
-    DB_HOST = os.environ.get("DB_HOST")
-    DB_PORT = os.environ.get("DB_PORT")
-    DB_NAME = os.environ.get("DB_NAME")
-    DB_USER = os.environ.get("DB_USER")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
-
     # Redis
     REDIS_HOST = os.environ.get("REDIS_HOST")
     REDIS_PORT = os.environ.get("REDIS_PORT")
@@ -65,13 +40,6 @@ else:
     # CELERY settings
     CELERY_BROKER_URL = "redis://{}:{}/0".format(REDIS_HOST, REDIS_PORT)
     CELERY_RESULT_BACKEND = "redis://{}:{}/0".format(REDIS_HOST, REDIS_PORT)
-
-    # Email
-    EMAIL_HOST = os.environ.get("EMAIL_HOST")
-    EMAIL_PORT = os.environ.get("EMAIL_PORT")
-    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 
     # Secret_key
     # SECURITY WARNING: keep the secret key used in production secret!
