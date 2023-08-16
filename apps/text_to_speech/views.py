f95c1a2a4d2461bc18a847cca8b2101476fbb8df
Mahadi Hassan <itsmahadi@gmail.com>
2023-08-16 06:50:25 +0600

    bug fixed

--- apps/text_to_speech/views.py
+++ apps/text_to_speech/views.py
@@ -1,11 +1,12 @@
+import os
 import uuid
 
-from bark import generate_audio
-from django.core.files.base import ContentFile
+from bark import generate_audio, SAMPLE_RATE
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from scipy.io.wavfile import write as write_wav
 
 from apps.text_to_speech.models import TextToSpeechModel
 from apps.text_to_speech.serializers import TextToSpeechModelSerializerDetails
@@ -43,12 +44,30 @@ def txt_to_audio_view(request):
         audio_array = generate_audio(text_prompt)
 
         # Generate a unique filename
-        unique_filename = f"{uuid.uuid4().hex[:10]}_generated_audio.wav"
+        audio_filename = f"{uuid.uuid4().hex[:10]}_generated_audio.wav"
+
+        # Save audio to disk
+        write_wav(audio_filename, SAMPLE_RATE, audio_array)
+
+        # # Create an instance of TextToSpeech model and save it
+        # text_to_speech_instance = TextToSpeechModel.objects.create(text=text)
+        # text_to_speech_instance.audio.save(audio_filename, open(audio_filename, 'rb'))
+        # text_to_speech_instance.save()
 
         # Create an instance of TextToSpeech model and save it
-        text_to_speech_instance = TextToSpeechModel(text=text)
-        audio_content = ContentFile(audio_array.tobytes())
-        text_to_speech_instance.audio.save(unique_filename, audio_content)
+        text_to_speech_instance = TextToSpeechModel.objects.create(text=text)
+        audio_content = open(audio_filename, 'rb')
+        text_to_speech_instance.audio.save(audio_filename, audio_content)
+        audio_content.close()
+
+        # Delete the temporary audio file
+        os.remove(audio_filename)
+
+        # Create an instance of TextToSpeech model and save it
+        # text_to_speech_instance = TextToSpeechModel.objects.create(text=text)
+        # audio_content = ContentFile(audio_array.tobytes())
+        # text_to_speech_instance.audio.save(unique_filename, audio_content)
+        # text_to_speech_instance.save()
 
         serialize_obj = TextToSpeechModelSerializerDetails(text_to_speech_instance, context={"request": request})
 
