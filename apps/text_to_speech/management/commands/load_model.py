93fe4bd13806a6fd6373492b824343b50daf7f65
Mahadi Hassan <itsmahadi@gmail.com>
2023-08-16 08:51:05 +0600

    Update load_model.py

--- apps/text_to_speech/management/commands/load_model.py
+++ apps/text_to_speech/management/commands/load_model.py
@@ -1,3 +1,4 @@
+import torch
 from bark import preload_models
 from django.core.management.base import BaseCommand
 
@@ -6,4 +7,6 @@ class Command(BaseCommand):
     help = "Add Sample Data"
 
     def handle(self, *args, **options):
+        torch.cuda.empty_cache()
         preload_models()
+        print("Models Loaded")
